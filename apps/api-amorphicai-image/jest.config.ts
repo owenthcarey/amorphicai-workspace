ac14a0ab44c29320683445304aaf014dd131cd6a
Owen Carey <owenthcarey@gmail.com>
2023-05-04 17:23:00 -0700

    Fifth commit

---
+++ apps/api-amorphicai-image/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'api-amorphicai-image',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api-amorphicai-image',
+};
--- apps/api-amorphicai/src/app/app.service.ts
+++ apps/api-amorphicai/src/app/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../../../../libs/xplat/core/src/lib/models/user.model';
+// import { User } from '@amorphicai-workspace/xplat/core';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    const user = new User();
+    console.log('User:', user);
     return { message: 'Hello API' };
   }
 }
